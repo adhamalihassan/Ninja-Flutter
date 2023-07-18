7f12778ce8b65c69d9f97d5af32fb3ae276e229a
adhamali <aali@sahlpay.app>
2023-07-18 23:26:32 +0300

    lesson 15

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,98 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home()));
+  runApp(MaterialApp(home: NinjaCard()));
 }
 
-class Home extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text(
-          'First App',
-          style: TextStyle(
-              fontFamily: 'IndieFlower',
-              fontWeight: FontWeight.bold,
-              fontSize: 30.0),
-        ),
-        backgroundColor: Colors.red[600],
+        title: Text('Ninja ID Card'),
         centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-            child: Image.asset('assets/download.jpeg'),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/weasly.jpeg'),
+                radius: 50.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'Name'.toUpperCase(),
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Bonnie Wright',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'Current Ninja Level'.toUpperCase(),
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'bonniewright@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    letterSpacing: 1.0,
+                    fontSize: 18.0,
+                  ),
+                )
+              ],
+            )
+          ],
         ),
-        onPressed: () {},
-        backgroundColor: Colors.red[600],
       ),
     );
   }
